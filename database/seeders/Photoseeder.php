31da254580875e9478163a271659fda66d02b517
Subir008 <subirkarmakar8aug@gmail.com>
2024-12-03 13:37:50 +0530

    Seeder file

--- database/seeders/Photoseeder.php
+++ database/seeders/Photoseeder.php
@@ -2,23 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\photo;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
-
-class StudentSeeder extends Seeder
+class Photoseeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('students')->insert([
-            'name' =>Str::random(5),
-            'email' =>Str::random(5,).'@gmail.com',
-            'contact' =>Str::random(5),
+        //
+        photo::insert([
+            'name'=> Str::random(5)
         ]);
     }
 }
