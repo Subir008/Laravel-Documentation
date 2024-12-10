1737fe4beb9b2f08971aea535f35b589c9b82c35
Subir008 <subirkarmakar8aug@gmail.com>
2024-12-10 22:22:45 +0530

    api

--- resources/views/api/api.blade.php
+++ resources/views/api/api.blade.php
@@ -8,5 +8,20 @@
         After running the command we can access the files in the <b>routes/api.php</b>
         <br>
         The routing path will be written in the <b>api.php</b> file of the routes folder.
+
+        <br><br>
+
+        <h3>Fetching all the data Using Api</h3>
+        <h4>Controller code -</h4>
+        function get_data(){    <br>
+        $student = new Student();    <br>
+        return $student->all(); <br>
+        }   <br>
+
+        Call the function in the api route path.
+
+        <h3>Adding data</h3>
+        
+
     </p>
 </div>
